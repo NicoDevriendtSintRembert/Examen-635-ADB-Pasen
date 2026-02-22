e9d7494edec889e6ffe53f563ae5e1549a613f42
Nico Devriendt <nico.devriendt@sint-rembert.be>
2026-02-22 18:14:59 +0100

    Initial commit

---
+++ Examen_ADB_635_Pasen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_ADB_635_Pasen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Zeeslag!");
+        }
+    }
+}
